e4b963b59923522229b21d2a4c7324e07dda716d
mitchelle.js <mitchellenjoki7@gmail.com>
2023-11-05 21:28:41 +0300

    textual data

---
+++ ShoppingCart01.java
@@ -0,0 +1,17 @@
+public class ShoppingCart01 {
+    public static void main(String[] args) {
+        // Declare and initialize String variables.  Do not initialize message yet.
+        String custName = "Alex";
+        String item = "shirt";
+        
+        
+        
+        
+        // Assign the message variable 
+         String message =custName + " wants to purchase " + item;
+        
+        // Print and run the code
+        System.out.println(message);
+        
+    }
+}
---
+++ ShoppingCart02.java
@@ -0,0 +1,25 @@
+public class ShoppingCart02 {
+    public static void main(String[] args) {
+        String custName = "Alex";
+        String itemDesc = "Shirts";
+        String message = custName+" wants to purchase a "+itemDesc;
+        
+        // Declare and initialize numeric fields: price, tax, quantity.   
+        double price = 100.00;
+        double tax = price * 0.5;
+        int quantity = 2;
+        
+        // Declare and assign a calculated totalPrice
+         double totalP = price * tax ;
+         double totalprice = totalP * quantity;
+        
+        // Modify message to include quantity 
+
+        message = custName+ " wants to purchse " + quantity +  " " + itemDesc + " the total price is : " +totalprice;
+        
+        System.out.println(message);
+        
+        // Print another message with the total cost
+        
+    }    
+}
